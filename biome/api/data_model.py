4063b9bd492c763dd572ca00a7a364ac0353787b
RedJane <evgenia.teml@gmail.com>
2014-11-19 18:42:56 +0400

    a line was added

--- biome/api/data_model.py
+++ biome/api/data_model.py
@@ -367,6 +367,7 @@ class Compound(Node):
 
     def __init__(self, name, uid=None, chemical_formula=None, smiles=None,
                  type=None, molecular_weight=None):
+        Node.__init__(self)
         self.uid = uid
         self.name = name
         self.chemical_formula = chemical_formula

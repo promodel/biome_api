341f07171ad966adccf1190390ed3c9acf5a3428
RedJane <evgenia.teml@gmail.com>
2015-03-29 15:54:52 +0300

    changes in the RegulonDB class. Starting to write a method for operons creation
    Former-commit-id: 4da38e6ec2d6937948f5c1642b8023714bcb06d6

--- biome/load/regulon/regulondb.py
+++ biome/load/regulon/regulondb.py
@@ -1,13 +1,62 @@
-from ...api import *
+#from ...api import *
+from py2neo import neo4j, node, rel, cypher
+import biome.load.genbank.genbank as gb
+import os
 
 class RegulonDB():
     """
 
     """
-    def __init__(self, ecoli_node, connection='http://localhost:7474/db/data/'):
-        if not isinstance(ecoli_node, Node):
-            raise TypeError('The ecoli_node argument must be a Node!')
-        if not isinstance(connection, basestring):
+    def __init__(self, directory, ecoli_name='Escherichia coli str. K-12 substr. MG1655',
+                 dblink='http://localhost:7474/db/data/'):
+        if not isinstance(ecoli_name, basestring):
+            raise TypeError('The ecoli_name argument must be a string!')
+        if not isinstance(dblink, basestring):
             raise TypeError('The connection argument must be a string!')
+        if not os.path.isdir(directory):
+            raise ValueError('The directory does not exist!')
+        self.directory = directory
+        self.ecoli_name = ecoli_name
+        self.dblink = dblink
+        self.connection = neo4j.GraphDatabaseService(self.dblink)
+
+        try:
+            # session = cypher.Session(self.dblink)
+            # transaction = session.create_transaction()
+            # query = 'MATCH (org:Organism {name:"%s"}) ' \
+            #         'RETURN org' % self.ecoli_name
+            # transaction.append(query)
+            # transaction_res = transaction.commit()[0]
+            ecoli_node = list(self.connection.find('Organism', 'name', self.ecoli_name))
+            # print transaction_res[0][0]
+        except:
+            raise ValueError('Check the dblink! Could not connect!')
+
+        if not ecoli_node:
+            raise ValueError('There is no organism node with %s name!' % self.ecoli_name)
+
         self.ecoli_node = ecoli_node
-        self.connection = connection
+
+
+    def __repr__(self):
+        return "RegulonDB object for %s\nLink to database: %s" \
+               % (self.ecoli_name, self.dblink)
+
+    def __str__(self):
+        return "RegulonDB object for %s\nLink to database: %s" \
+               % (self.ecoli_name, self.dblink)
+
+    def create_operons(self):
+        f = open(self.directory + 'Operons.txt', 'r')
+        data = f.readlines()
+        f.close()
+        for line in data:
+            if line[0] == '#':
+                continue
+            chunks = line.split('\t')
+
+
+
+
+
+

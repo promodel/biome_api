d4fff2220edd5d42a789c31bfb9619314a84d98d
RedJane <evgenia.teml@gmail.com>
2015-06-02 19:22:42 +0300

    a NewReference class was created
    Former-commit-id: 799cba8657a8f29f4531479f9886d3219dcbe94c

--- biome/load/utils/location_update.py
+++ biome/load/utils/location_update.py
@@ -1,1 +1,30 @@
- 
+import re
+import os
+import warnings
+
+class NewReference():
+    """
+    """
+    def __init__(self, new_reference, old_reference, path='./'):
+        new_reference = new_reference.upper()
+        old_reference = old_reference.upper()
+        if not isinstance(path, basestring):
+            raise TypeError('The path argument must be a string!')
+        if not isinstance(new_reference, basestring):
+            raise TypeError('The new_reference argument must be a string!')
+        if not isinstance(old_reference, basestring):
+            raise TypeError('The old_reference argument must be a string!')
+        if not isinstance(old_reference, basestring):
+            raise TypeError('The old_reference argument must be a string!')
+        if not set(list(new_reference) + list(old_reference)) <= set(['A', 'T', 'G', 'C']):
+            warnings.warn('Sequences have noncanonical nucleotides!')
+        self.new_ref = new_reference
+        self.old_ref = old_reference
+        self.path = path
+
+    def __repr__(self):
+        return "_DatSet constructed for %s file" % self.filename
+
+    def __str__(self):
+        return "_DatSet constructed for %s file" % self.filename
+

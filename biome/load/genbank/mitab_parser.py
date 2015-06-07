3e374f9334bd3bc588c5ac654001d24e24c243fa
arc7an <arc7an@gmail.com>
2015-06-08 00:39:22 +0300

    Create dictionary out of MITAB-format description.
    Former-commit-id: b89fbcca79a24cb171e86aaf0c7bab58bf42abd7

--- biome/load/genbank/mitab_parser.py
+++ biome/load/genbank/mitab_parser.py
@@ -1,21 +1,28 @@
 def parse_mitab(mitab_file):
+    # read mitab file
     open_mitab_file = open(mitab_file, 'r')
     text = open_mitab_file.readlines()
     open_mitab_file.close()
+    # split headers of the table and data
     headers = text[0].split('\n')
     values = text[1].split('\n')
     table_of_headers = headers[0].split('\t')
     table_of_values = values[0].split('\t')
+    # parse and upload it
     for header, value in zip(table_of_headers, table_of_values):
         print header, '\n', value, '\n'
 #         to be implemented
 #         compare with big dictionary and upload current data to DB
 
 def get_mi_dict(dict_file):
+    # read format
     open_file = open(dict_file, 'r')
     text = open_file.readlines()
     open_file.close()
+    # cut the description
     text = text[text.index('[Term]\n'):]
+    interaction_dict = {}
+    # read short parts for each term
     while text[0] == '[Term]\n':
         end_index = text.index('\n')+1
         part = text[0:end_index]
@@ -30,6 +37,8 @@ def get_mi_dict(dict_file):
                     part_dict[key] = value
             except:
                 continue
+            interaction_dict[part_dict['id']] = part_dict
         #     to be implemented
         #     create big dictionary
         text = text[end_index:]
+    return interaction_dict

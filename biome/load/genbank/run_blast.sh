17dc900d429b457a97050de34a623ad2855dc9c0
arc7an <arc7an@gmail.com>
2015-11-13 19:24:16 +0300

    New structure of blast graph-presentation
    Former-commit-id: 029bd849563d48a1db26751af0a9f08ff9ea1a16

--- biome/load/genbank/run_blast.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-blastp -db $2 -evalue 0.000010 -out "Chlamydia trachomatis A2497_input_blast_blast_out_part$1.xml" -query "Chlamydia trachomatis A2497_input_blast_part$1.FASTA" -outfmt 5 
--- biome/load/genbank/upload.py
+++
@@ -1,6 +0,0 @@
-import genbank as gb
-
-gb_file = '/home/artem/work/Biolum/biolum_bacs/full_genomes/Aliivibrio salmonicida LFI1238 plasmid pVSAL840, complete sequence.gb'
-connection = gb.BioGraphConnection()
-organism = gb.GenBank(gb_file, connection)
-organism.upload()
